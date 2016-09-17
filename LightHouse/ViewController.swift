4c0125840376847d5e95f245d9600bb8fd0cd71c
jason.mcaffee <jason.mcaffee@insidesales.com>
2016-09-17 14:06:50 -0600

    button wired up. CTRL Drag button to ViewCOntroller.  select type ACTION

--- LightHouse/ViewController.swift
+++ LightHouse/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var getStateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Actions
 
+    @IBAction func getStateButtonTouched(sender: UIButton) {
+        print("BUTTON PRESSED");
+    }
 }
 
