80e85c45176078a2f4ae0e3af51bd19a1423e11f
jasonlmcaffee@gmail.com <jasonlmcaffee@gmail.com>
2016-09-17 22:00:54 -0600

    fix for some state properties being null

--- LightHouse/State.swift
+++ LightHouse/State.swift
@@ -14,16 +14,16 @@ import EVReflection
  * State of a Hue Light (bulb, go, etc)
  */
 class State : EVObject{
-    var on  :Bool?;         //true
-    var bri :Int?;          //1
-    var hue :Int?;          //3271
-    var sat :Int?;          //244
+    var on  :Bool = false;         //true
+    var bri :Int = 0;          //1
+    var hue :Int = 0;          //3271
+    var sat :Int = 0;          //244
     var effect :String?;    //none
     var xy : Array<Double>?; // [0.6397, 0.3482]
-    var ct :Int?;           //153
+    var ct :Int = 0;           //153
     var alert :String?;     //none
     var colormode: String?; //hs
-    var reachable: Bool?;// true
+    var reachable: Bool = false;// true
     
     required init(){}
 }
